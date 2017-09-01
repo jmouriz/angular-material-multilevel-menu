89886ab8fa83ba033368020e2a0ffcd2d6a3c35d
Juan Manuel <jmouriz@gmail.com>
2017-08-31 21:22:25 -0300

    Final approach

--- demo/demo.js
+++ demo/demo.js
@@ -1,8 +1,7 @@
-var demo = angular.module('Demo', ['ngMaterial', 'ngAnimate', 'ngMdIcons', 'ngMdMultiLevelMenu']);
+var demo = angular.module('Demo', ['ngMaterial', 'ngMdMultiLevelMenu']);
 
-demo.controller('Demo', ['$scope', '$animate', function($scope, $animate) {
-
-   $scope.menu = [{
+demo.config(['menuProvider', function(menuProvider) {
+   menuProvider.set([{
       item: 'Item 1',
       menu: [{
          item: 'Item 1'
@@ -25,52 +24,5 @@ demo.controller('Demo', ['$scope', '$animate', function($scope, $animate) {
       }]
    }, {
       item: 'Item 4'
-   }];
-
-   $scope.select = function(item) {
-      $scope.selected = item.item;
-   };
-
-   $scope.reset = function() {
-      $debug = $animate;
-      $scope.stack = [];
-      $scope.current = {
-         item: 'Menú principal',
-         menu: $scope.menu
-      };
-   };
-
-   $scope.click = function(item) {
-      if (item.menu) {
-         var menu = angular.element('div.menu');
-         $animate.addClass(menu, 'left').then(function() {
-            $scope.stack.push($scope.current);
-            $scope.current = {
-               item: item.item,
-               menu: item.menu
-            }
-            menu.removeClass('ng-animate');
-            menu.removeClass('left');
-            menu.addClass('right');
-            menu.addClass('ng-animate');
-            $animate.removeClass(menu, 'right');
-         });
-      }
-      $scope.select(item);
-   };
-
-   $scope.back = function(index) {
-      var left = $scope.stack.length - index;
-      do var previous = $scope.stack.pop();
-      while (--left);
-      var menu = angular.element('div.menu');
-      $animate.addClass(menu, 'right').then(function() {
-         $scope.current = previous;
-         menu.removeClass('ng-animate');
-         menu.removeClass('right');
-         menu.addClass('left');
-         menu.addClass('ng-animate');
-         $animate.removeClass(menu, 'left');
-      });
-   };
+   }]);
 }]);
--- source/angular-material-multilevel-menu.js
+++ source/angular-material-multilevel-menu.js
@@ -1,14 +1,76 @@
 (function(window, angular, undefined) {
    'use strict';
+
+   var module = angular.module('ngMdMultiLevelMenu', ['ngMaterial', 'ngAnimate', 'ngMdIcons']);
+
+   module.provider('menu', function() {
+		this.menu = [];
+
+      this.$get = function() {
+         return this.menu;
+      };
+
+		this.set = function(menu) {
+			this.menu = menu;
+		};
+   });
+
+   module.controller('MenuController', ['$scope', '$animate', 'menu', function($scope, $animate, menu) {
+      $scope.menu = menu;
+
+      $scope.select = function(item) {
+         $scope.selected = item.item;
+      };
    
-   angular.module('ngMdMultiLevelMenu', ['ngMaterial', 'ngAnimate', 'ngMdIcons']).directive('mdMultiLevelMenu', [function() {
+      $scope.reset = function() {
+         $scope.stack = [];
+         $scope.current = {
+            item: 'Menú principal',
+            menu: $scope.menu
+         };
+      };
+   
+      $scope.click = function(item) {
+         if (item.menu) {
+            var menu = angular.element('div.menu');
+            $animate.addClass(menu, 'left').then(function() {
+               $scope.stack.push($scope.current);
+               $scope.current = {
+                  item: item.item,
+                  menu: item.menu
+               }
+               menu.removeClass('ng-animate');
+               menu.removeClass('left');
+               menu.addClass('right');
+               menu.addClass('ng-animate');
+               $animate.removeClass(menu, 'right');
+            });
+         }
+         $scope.select(item);
+      };
+   
+      $scope.back = function(index) {
+         var left = $scope.stack.length - index;
+         do var previous = $scope.stack.pop();
+         while (--left);
+         var menu = angular.element('div.menu');
+         $animate.addClass(menu, 'right').then(function() {
+            $scope.current = previous;
+            menu.removeClass('ng-animate');
+            menu.removeClass('right');
+            menu.addClass('left');
+            menu.addClass('ng-animate');
+            $animate.removeClass(menu, 'left');
+         });
+      };
+   }]);
+   
+   module.directive('mdMultiLevelMenu', [function() {
       return {
          restrict: 'E',
          replace: true,
-         transclude: true,
-         template: function(element, attributes) {
-            return '<div ng-transclude></div>';
-         }
+         controller: 'MenuController',
+         templateUrl: 'angular-material-multilevel-menu.min.html'
       };
    }]);
 })(window, window.angular, undefined);
